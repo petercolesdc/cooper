146aaccf6ea7cee5a73c8d983cdeb7cd9bc142fb
Pete Coles <pete@fffunction.co>
2017-11-28 00:17:41 +0000

    Base setup

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# cooper
-Style layer base
+# Cooper
+
+Base style and functional layers. Dale Cooper approved.
+![Dale Cooper Approved](https://i.imgur.com/ZDWFHnB.jpg "Dale Cooper Approved")
+
+## What is?
+
+Default SCSS, JS and assets for starting new projects
+
+## How
+
+It all runs on gulp and inputs and outputs are configured via the gulpfile.js.
+As a default, there are templates bundled with the build and a local server setup from flat files. This is to test elements, and you wont need it in you builds on on other projects.
+
+## Render
+
+Run 'gulp' from root
+
+
+## Test elements
+
+If you're updating this repo, to test elements just run 'gulp serve' from the root.
---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp            = require("gulp")
+    sass            = require("gulp-sass")
+    autoprefixer    = require("gulp-autoprefixer")
+    sassGlob        = require('gulp-sass-glob')
+    del             = require("del")
+    nunjucksRender  = require("gulp-nunjucks-render")
+    fs              = require("fs")
+    runSequence     = require("run-sequence")
+    exec            = require("child_process").exec;
+    browserSync     = require("browser-sync").create();
+
+    // Concat and copy CSS
+    gulp.task("scss", function () {
+      gulp.src("scss/**/*.scss")
+        .pipe(sassGlob())
+        .pipe(sass({outputStyle : "compressed"}))
+        .pipe(autoprefixer({browsers : ["last 6 versions"]}))
+        .pipe(gulp.dest("public/css/"))
+    })
+
+    // Copy javascript
+    gulp.task("js", function () {
+      gulp.src("js/**/*.js")
+        .pipe(gulp.dest("public/js/"))
+    })
+
+    // Assets copy
+    gulp.task("assets", function () {
+      gulp.src("assets/**/*")
+        .pipe(gulp.dest("public/assets/"))
+    })
+
+    // Watch asset folder for changes
+    gulp.task("watch", ["scss", "js", "assets"], function () {
+      gulp.watch("js/**/*", ["js"])
+      gulp.watch("scss/**/*", ["scss"])
+      gulp.watch("assets/**/*", ["assets"])
+    })
+
+    // --------------------------------------
+    // Test templates
+    // --------------------------------------
+
+    // Render templates
+    gulp.task("render", function () {
+      gulp.src("templates/*.html")
+        .pipe(nunjucksRender({
+          path: ["templates"]
+        }))
+        .pipe(gulp.dest("public"))
+    })
+
+    // Watch more things
+    gulp.task("watch-all", ["scss", "js", "assets", "render"], function () {
+      gulp.watch("js/**/*", ["js"])
+      gulp.watch("scss/**/*", ["scss"])
+      gulp.watch("assets/**/*", ["assets"])
+      gulp.watch("templates/**/*", ["render"])
+    })
+
+    // Spin up server
+    gulp.task('browser-sync', function() {
+        browserSync.init({
+            server: {
+                baseDir: "public",
+            },
+            notify: false
+        });
+        gulp.watch("public/**/*").on('change', browserSync.reload);
+    });
+
+    // --------------------------
+    // Task runners syntax
+    // --------------------------
+
+    gulp.task("default", ["watch"])
+
+    // Spins up a sever to render test templates
+    gulp.task("serve", ["watch-all", "browser-sync"])
